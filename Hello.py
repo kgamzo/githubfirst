199c623807fc9a324ca43d2e6d536c162c6922f6
Maxwell <kgamzo@gmail.com>
2020-07-11 18:15:02 +0200

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,10 @@
+print ('Hello World')
+print ('what is your name?') # Asks for your their name
+Myname = input()
+ print ('Its good to meet your, ' + Myname)
+print ('The length of your name is:')
+print(len(Myname)) 
+print ('What is your age')
+myage = input()
+print('you will be ' + str(int(myage) + 1) + ' in a year.')
+       
